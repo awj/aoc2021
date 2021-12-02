e0f2204adb525c7cddd6c49fe98f3636461459e7
Adam Jones <ajones1@gmail.com>
2021-12-02 08:22:13 -0700

    Day 2, star 2.

--- lib/day2.ex
+++ lib/day2.ex
@@ -1,28 +1,33 @@
 defmodule Day2 do
   defmodule Location do
-    defstruct position: 0, depth: 0
+    defstruct position: 0, depth: 0, aim: 0
 
     def forward(location, val) do
-      %{location | position: location.position + val}
+      # Depth changes by `aim` once for each unit we move forward. (So moving
+      # forward 3 with an aim of 5 would result in depth + 15)
+      new_depth = location.depth + location.aim * val
+      %{location | position: location.position + val, depth: new_depth}
     end
 
     def down(location, val) do
-      %{location | depth: location.depth + val}
+      %{location | aim: location.aim + val}
     end
 
     def up(location, val) do
-      %{location | depth: location.depth - val}
+      %{location | aim: location.aim - val}
     end
 
-    def apply(location, "forward", val) do
+    # Deep structural matching keeps the code short / easy to read, but *really*
+    # ties us to the format of the input text.
+    def apply(location, ["forward", val]) do
       Location.forward(location, String.to_integer(val))
     end
 
-    def apply(location, "down", val) do
+    def apply(location, ["down", val]) do
       Location.down(location, String.to_integer(val))
     end
 
-    def apply(location, "up", val) do
+    def apply(location, ["up", val]) do
       Location.up(location, String.to_integer(val))
     end
 
@@ -33,7 +38,7 @@ defmodule Day2 do
     def process(input) do
       String.split(String.trim(input), "\n")
       |> Stream.map(&(String.split(&1, " ")))
-      |> Enum.reduce(%Location{}, fn ([dir, val], location) -> Location.apply(location, dir, val) end)
+      |> Enum.reduce(%Location{}, &Location.apply/2)
     end
   end
 end
