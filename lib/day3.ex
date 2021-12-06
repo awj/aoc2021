351a0b683c500c2ddb0a0de18821a7d0714b33e5
Adam Jones <ajones1@gmail.com>
2021-12-06 10:31:18 -0700

    :no-idea-dog:

--- lib/day3.ex
+++ lib/day3.ex
@@ -25,7 +25,7 @@ defmodule Day3 do
   end
 
   def combine({zero, one}, {zero_digits, one_digits}) do
-    if zero > one do
+    if zero < one do
       {zero_digits ++ [1], one_digits ++ [0]}
     else
       {zero_digits ++ [0], one_digits ++ [1]}
@@ -36,15 +36,54 @@ defmodule Day3 do
     zero_values = harvest_values(zeroes)
     one_values = harvest_values(ones)
 
-    {zero_digits, one_digits} = Enum.zip(zero_values, one_values) |> Enum.reduce({[], []}, &Day3.combine/2)
+    Enum.zip(zero_values, one_values) |> Enum.reduce({[], []}, &Day3.combine/2)
+  end
 
-    { Integer.undigits(zero_digits, 2), Integer.undigits(one_digits, 2) }
+  def build_tree(input, tree \\ %{})
+
+  def build_tree([input], tree) do
+    {old_val, final_tree} = append_tree(
+      String.graphemes(input) |> Enum.map(&String.to_integer/1),
+      input,
+      tree
+    )
+
+    final_tree
+  end
+
+  def build_tree([input | inputs], tree) do
+    {old_val, new_tree} = append_tree(
+      String.graphemes(input) |> Enum.map(&String.to_integer/1),
+      input,
+      tree
+    )
+
+    build_tree(inputs, new_tree)
+  end
+
+  def append_tree([final], input, tree) do
+    Map.get_and_update(tree, final, fn current_value ->
+      {current_value, [input | (current_value || [])]}
+    end)
+  end
+
+  def append_tree([val | values], input, tree) do
+    Map.get_and_update(tree, val, fn current_value ->
+      {current_value, elem(append_tree(values, input, current_value || %{}), 1)}
+    end)
+  end
+
+  def value_for(val_tree, [final_bit]) do
+    Map.get(val_tree, final_bit)
+  end
+
+  def value_for(val_tree, [bit | bits]) do
+    value_for(Map.get(val_tree, bit), bits)
   end
 
   def power_consumption(input) do
-    {gamma, epsilon} = bit_usage(input)
-     |> checksums
+    {gamma, epsilon} = Day3.bit_usage(input)   |> Day3.checksums
 
-    gamma * epsilon
+    Integer.undigits(gamma, 2) * Integer.undigits(epsilon, 2)
   end
 end
