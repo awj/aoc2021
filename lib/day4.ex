648bd1fa55d7be44c88c8b7eb7c7ac901290c553
Adam Jones <ajones1@gmail.com>
2021-12-06 23:09:08 -0700

    Day 4, Star 2.

--- lib/day4.ex
+++ lib/day4.ex
@@ -27,9 +27,9 @@ defmodule Day4 do
     end
 
     def column_winner?(board) do
-      Enum.any?((0..board.width), fn (i) ->
+      Enum.any?((1..board.width), fn (i) ->
         Enum.all?(board.contents, fn (row) ->
-          {_, marked} = Enum.at(row, i)
+          {_, marked} = Enum.at(row, i-1)
           marked
         end)
       end)
@@ -55,7 +55,7 @@ defmodule Day4 do
         end)
       end)
 
-      %Board{contents: new_contents }
+      %Board{ contents: new_contents, width: board.width }
     end
   end
 
@@ -101,6 +101,26 @@ defmodule Day4 do
         Board.score(winner, next_number)
       else
         winning_score(%Game{ numbers: tl(game.numbers), boards: updated_boards })
+      end
+    end
+
+    def all_winners(game, winning_boards \\ []) do
+      next_number = hd(game.numbers)
+
+      updated_boards = Enum.map(game.boards, fn (board) ->
+        Board.mark(board, next_number)
+      end)
+
+      {winners, remaining} = Enum.split_with(updated_boards, &Board.winner?/1)
+
+      IO.inspect(next_number, label: "next")
+      IO.inspect(winners, label: "new winners")
+      new_winners = winning_boards ++ Enum.map(winners, &(Board.score(&1, next_number)))
+
+      if length(tl(game.numbers)) == 0 do
+        new_winners
+      else
+        all_winners(%Game{ numbers: tl(game.numbers), boards: remaining }, new_winners)
       end
     end
   end
