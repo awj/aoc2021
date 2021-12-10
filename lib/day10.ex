41256d7d9e51d1beaa6e057c546b0a115d385335
Adam Jones <ajones1@gmail.com>
2021-12-10 11:04:16 -0700

    Day 10, star 2.

--- lib/day10.ex
+++ lib/day10.ex
@@ -18,6 +18,48 @@ defmodule Day10 do
     end
   end
 
+  # Pick the *middle* element (always an odd number) of the sorted list of
+  # autocorrection scores.
+  def autocorrect_winner(input) do
+    lines = String.split(input)
+
+    scores = Enum.map(lines, &autocorrect_score/1) |> Enum.filter(&(&1 != 0)) |> Enum.sort
+
+    size = Enum.count(scores)
+
+    middle = trunc(size / 2)
+
+    Enum.at(scores, middle)
+  end
+
+  def autocorrect_score(line) do
+    chars = String.graphemes(line)
+
+    case examine(chars, []) do
+      {:balanced, _} -> 0
+      {:incomplete, rest} -> completion_score(rest)
+      {:corrupted, _, _} -> 0
+    end
+  end
+
+  # Autocorrect completion is handled by passing a total along as we walk the
+  # stack of "leftover" symbols from examination. For each symbol, add a number
+  # to the score corresponding to the value for a given symbol's closing brace.
+  def completion_score(rest, total \\ 0)
+
+  def completion_score([], total), do: total
+
+  def completion_score([head | rest], total) do
+    increment = case head do
+      "(" -> 1
+      "[" -> 2
+      "{" -> 3
+      "<" -> 4
+                end
+
+    completion_score(rest, total * 5 + increment)
+  end
+
   # Evaluated nested expressions using a stack. General algorithm:
   # * Push all opening braces onto the stack
   # * For closing braces, pop the top of the stack
@@ -45,4 +87,4 @@ defmodule Day10 do
       _ -> {:corrupted, balance, closing }
     end
   end
-end
+ end
