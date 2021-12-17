aba5f7c7d4426b30fc03f105c94cb82fdf417f5d
Adam Jones <ajones1@gmail.com>
2021-12-17 13:21:16 -0700

    Relatively easy after all the parsing was done.

--- lib/day16.ex
+++ lib/day16.ex
@@ -38,12 +38,64 @@ defmodule Day16 do
     display(rest, result ++ [n])
   end
 
-  def version_total({:literal, version, _}) do
+  def version_total({:literal, version, _type, _value}) do
     version
   end
 
-  def version_total({_some_container, version, parts}) do
+  def version_total({_some_container, version, _type, parts}) do
     version + (Enum.map(parts, &version_total/1) |> Enum.sum)
+  end
+
+  def evaluate({_key, _version, 0, parts}) do
+    parts
+    |> Enum.map(&evaluate/1)
+    |> Enum.sum
+  end
+
+  def evaluate({_key, _version, 1, parts}) do
+    parts
+    |> Enum.map(&evaluate/1)
+    |> Enum.product
+  end
+
+  def evaluate({_key, _version, 2, parts}) do
+    parts
+    |> Enum.map(&evaluate/1)
+    |> Enum.min
+  end
+
+  def evaluate({_key, _version, 3, parts}) do
+    parts
+    |> Enum.map(&evaluate/1)
+    |> Enum.max
+  end
+
+  def evaluate({_key, _version, 4, val}) do
+    val
+  end
+
+  def evaluate({_key, _version, 5, [p1, p2 | _rest]}) do
+    if evaluate(p1) > evaluate(p2) do
+      1
+    else
+      0
+    end
+  end
+
+  def evaluate({_key, _version, 6, [p1, p2 | _rest]}) do
+    if evaluate(p1) < evaluate(p2) do
+      1
+    else
+      0
+    end
+  end
+
+  def evaluate({_key, _version, 7, [p1, p2 | _rest]}) do
+    if evaluate(p1) == evaluate(p2) do
+      1
+    else
+      0
+    end
   end
 
   def to_integer(bits) do
@@ -58,40 +110,40 @@ defmodule Day16 do
     parse_literal(version, rest, 6)
   end
 
-  def parse(<< version :: 3, _type :: 3, flag :: 1, rest :: bits>>) do
+  def parse(<< version :: 3, type :: 3, flag :: 1, rest :: bits>>) do
     if flag == 0 do
       << len :: bits-size(15), rest :: bits >> = rest
       len = to_integer(len)
 
-      parse_length_container(version, len, rest, 22)
+      parse_length_container(version, type, len, rest, 22)
     else
       << len :: bits-size(11), rest :: bits >> = rest
       len = to_integer(len)
 
-      parse_count_container(version, len, rest, 18)
+      parse_count_container(version, type, len, rest, 18)
     end
   end
 
-  def parse_count_container(version, len, rest, bits_consumed, parts \\ [])
+  def parse_count_container(version, type, len, rest, bits_consumed, parts \\ [])
 
-  def parse_count_container(version, 0, rest, bits_consumed, parts) do
-    container = {:count_container, version, parts}
+  def parse_count_container(version, type, 0, rest, bits_consumed, parts) do
+    container = {:count_container, version, type, parts}
 
     { container, bits_consumed, rest }
   end
 
-  def parse_count_container(version, len, rest, bits_consumed, parts) do
+  def parse_count_container(version, type, len, rest, bits_consumed, parts) do
     {element, consumed, remaining} = parse(rest)
 
-    parse_count_container(version, len - 1, remaining, bits_consumed + consumed, parts ++ [element])
+    parse_count_container(version, type, len - 1, remaining, bits_consumed + consumed, parts ++ [element])
   end
   
-  def parse_length_container(version, len, bits, bits_consumed) do
+  def parse_length_container(version, type, len, bits, bits_consumed) do
     << slice :: bits-size(len), rest :: bits >> = bits
 
     bits_consumed = bits_consumed + len
 
-    container = {:length_container, version, length_parts(slice)}
+    container = {:length_container, version, type, length_parts(slice)}
 
     { container, bits_consumed, rest }
   end
@@ -111,7 +163,7 @@ defmodule Day16 do
   def parse_literal(version, remainder, bits_consumed) do
     {val, consumed, remainder} = parse_literal_value(remainder, <<>>, bits_consumed)
 
-    element = {:literal, version, to_integer(val)}
+    element = {:literal, version, 4, to_integer(val)}
     {element, bits_consumed + consumed, remainder}
   end
 
