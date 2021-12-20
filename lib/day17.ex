9c17149562d14dfb12e07ab994539c67aa792726
Adam Jones <ajones1@gmail.com>
2021-12-20 14:44:56 -0700

    Day 17, star 2.
    Work by simulating the x and y movement and capturing "tick ranges" that
    define the ticks for which the x and y values are within their
    respective components of the target area. If we see the target area for
    both x and y values AND their tick ranges overlap, it's a valid vector.

--- lib/day17.ex
+++ lib/day17.ex
@@ -2,9 +2,14 @@ defmodule Day17 do
   def solutions(xrange, yrange) do
     for x <- 0..xrange.last,
       y <- (yrange.first - 1)..1_000,
-      {xstatus, ticks} = simulate_x(x, xrange),
+      {xstatus, xtick_range} = simulate_x(x, xrange),
       xstatus in [:reaches, :crossthrough],
-      y_reaches_range?(y, yrange, xstatus, ticks) do
+      # Pass along the maximum x tick value seen so we know to stop calculating
+      # if we start to tick past it.
+      {ystatus, ytick_range} = simulate_y(y, yrange, xtick_range.last),
+      ystatus in [:reaches, :crossthrough],
+      !Range.disjoint?(xtick_range, ytick_range)
+      do
       {x, y}
     end
   end
@@ -15,55 +20,66 @@ defmodule Day17 do
     series_velocity_distance(best_y)
   end
 
-  def y_reaches_range?(yvel, yrange, x_handling, x_ticks)
+  def simulate_y(yvel, yrange, max_x_ticks, y \\ 0, ticks \\ 0, crossed_at_tick \\ nil)
 
-  def y_reaches_range?(yvel, yrange, :reaches, _x_ticks) do
-    {result, _ticks} = simulate_y(yvel, yrange, :unlimited)
-
-    result == :reaches
-  end
-
-  def y_reaches_range?(yvel, yrange, :crossthrough, x_ticks) do
-    {result, _ticks} = simulate_y(yvel, yrange, x_ticks)
-
-    result == :reaches
-  end
-
-  def simulate_y(yvel, yrange, tick_limit, y \\ 0, ticks \\ 0)
-
-  def simulate_y(yvel, yrange, :unlimited, y, ticks) do
+  def simulate_y(yvel, yrange, max_x_ticks, y, ticks, nil) do
     cond do
-      y + yvel in yrange -> { :reaches, ticks + 1}
-      (yrange.last < 0 && y + yvel < yrange.last) -> {:misses, -1}
-      true -> simulate_y(yvel - 1, yrange, :unlimited, y + yvel, ticks + 1)
+      y in yrange -> simulate_y(yvel - 1, yrange, max_x_ticks, y + yvel, ticks + 1, ticks)
+      ticks > max_x_ticks -> {:misses, -1}
+      y < yrange.first -> {:misses, -1}
+      true -> simulate_y(yvel - 1, yrange, max_x_ticks, y + yvel, ticks + 1)
     end
   end
 
-  def simulate_y(yvel, yrange, tick_limit, y, ticks) do
+  def simulate_y(yvel, yrange, max_x_ticks, y, ticks, crossed_at_tick) do
     cond do
-      ticks > tick_limit -> {:misses, -1}
-      (ticks == tick_limit) && y + yvel in yrange -> {:reaches, ticks + 1}
-      (ticks == tick_limit) -> {:misses, -1}
-      true -> simulate_y(yvel - 1, yrange, tick_limit, y + yvel, ticks + 1)
+      y in yrange -> simulate_y(yvel - 1, yrange, max_x_ticks, y + yvel, ticks + 1, crossed_at_tick)
+      ticks > max_x_ticks -> {:crossthrough, crossed_at_tick..(ticks - 1)}
+      true -> {:crossthrough, crossed_at_tick..(ticks - 1)}
     end
   end
 
-  def simulate_x(xvel, xrange, x \\ 0, ticks \\ 0)
+  def simulate_x(xvel, xrange, x \\ 0, ticks \\ 0, crossed_at_tick \\ nil)
 
-  def simulate_x(xvel, xrange, x, ticks) when xvel in [0, 1] do
-    if x + xvel in xrange do
-      {:reaches, -1}
-    else
-      {:misses, -1}
-    end
+  # If we reach zero velocity without ever crossing into the x range, the
+  # initial velocity was just bad.
+  def simulate_x(0, _xrange, _x, _ticks, nil) do
+    {:misses, -1}
   end
 
-  def simulate_x(xvel, xrange, x, ticks) do
+  # If we reach zero velocity while within the x range, we'll stay in range for
+  # all future ticks.
+  def simulate_x(0, _xrange, _x, _ticks, crossed_at_tick) do
+    {:reaches, crossed_at_tick..1_000_000}
+  end
+
+  # When we haven't (yet) crossed into the range:
+  # * if we cross into it, note this as the starting tick and continue simulating with that knowledge
+  # * if we pass the range entirely, we "skipped" it and missed our change
+  # * otherwise, continue simulating by deriving the next x position and the next tick
+  def simulate_x(xvel, xrange, x, ticks, nil) do
     cond do
+      x in xrange -> simulate_x(xvel - 1, xrange, x + xvel, ticks + 1, ticks)
       x > xrange.last -> {:misses, -1}
-      x + xvel > xrange.last && x in xrange -> {:crossthrough, ticks}
       true -> simulate_x(xvel - 1, xrange, x + xvel, ticks + 1)
     end
+  end
+
+  # When we have crossed into the range:
+  # * if we cross out it, return a crossthrough with the range of ticks from when we started and left
+  # * otherwise, continue simulating by deriving the next x position and the next tick
+  def simulate_x(xvel, xrange, x, ticks, crossed_at_tick) do
+    cond do
+      x in xrange -> simulate_x(xvel - 1, xrange, x + xvel, ticks + 1, crossed_at_tick)
+      true -> {:crossthrough, crossed_at_tick..(ticks - 1)}
+    end
+  end
+
+  def distinct_velocities(solutions) do
+    solutions
+    |> Stream.flat_map(&Tuple.to_list/1)
+    |> Stream.uniq
+    |> Enum.count
   end
 
   # In either the X direction or vertical Y direction, we end up with a
